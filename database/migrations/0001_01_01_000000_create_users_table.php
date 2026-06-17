7301d1aa3ee081ecf4eaaf2d53c3c14db43343ea
pisitchai <pisitchai1994@gmail.com>
2026-06-17 15:02:09 +0700

    chore:schema users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,27 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('name', 100)->nullable();
+            $table->string('email', 150)->unique('idx_email');
+            $table->string('username', 100)->nullable()->unique('users_username_unique');
+            $table->string('password', 255);
+            $table->enum('role', ['user', 'admin'])->default('user');
             $table->timestamps();
-        });
 
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
-        });
-
-        Schema::create('sessions', function (Blueprint $table) {
-            $table->string('id')->primary();
-            $table->foreignId('user_id')->nullable()->index();
-            $table->string('ip_address', 45)->nullable();
-            $table->text('user_agent')->nullable();
-            $table->longText('payload');
-            $table->integer('last_activity')->index();
+            $table->index('role', 'idx_role');
         });
     }
 
