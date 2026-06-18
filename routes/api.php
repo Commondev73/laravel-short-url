016d4762c03e25dcab6fded60930853bf5147ef8
pisitchai <pisitchai1994@gmail.com>
2026-06-19 02:16:36 +0700

    feat(shortUrl):api delete

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,7 @@ Route::prefix('short-urls')->group(function (): void {
     Route::middleware('auth:api')->group(function (): void {
         Route::get('/', [ShortUrlController::class, 'index']);
         Route::post('/', [ShortUrlController::class, 'store']);
-        Route::patch('/{id}', [ShortUrlController::class, 'update']);
+        Route::put('/{id}', [ShortUrlController::class, 'update']);
+        Route::delete('/{id}', [ShortUrlController::class, 'destroy']);
     });
 });
